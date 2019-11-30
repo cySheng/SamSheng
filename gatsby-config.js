540e3fc94318674913906bfdc03967e2ce26ec92
Samantha Lau <samantha.lhe@gmail.com>
2019-11-30 16:35:05 +0800

    Add sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,9 +12,9 @@ module.exports = {
          * The base URL of the WordPress site without the trailingslash and the protocol. This is required.
          * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
          */
-        baseUrl: "www.samsheng.my",
+        baseUrl: process.env.GATSBY_WP_URL,
         // The protocol. This can be http or https.
-        protocol: "https",
+        protocol: process.env.GATSBY_WP_PROTOCOL,
         // Indicates whether the site is hosted on wordpress.com.
         // If false, then the assumption is made that the site is self hosted.
         // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
