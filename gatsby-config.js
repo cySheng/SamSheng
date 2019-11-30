95e33808a114260f62b2c1fe7d6d2bf514959939
Samantha Lau <samantha.lhe@gmail.com>
2019-11-30 16:28:01 +0800

    Add site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `SamSheng`,
+    description: `The writings of Sammie and Sheng`,
+    author: `Samantha Lau & Chang Yu Sheng`,
   },
   plugins: [
     {
